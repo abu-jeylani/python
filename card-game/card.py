8f3ad3f11c33f54d258a255f8158fe730d715140
abu <abu.mohamed@phoenix.edu>
2023-08-26 18:33:12 -0600

    cleanup and adding card-game code

--- card-game/card.py
+++ card-game/card.py
@@ -1,11 +1,19 @@
+
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 
+         'Seven', 'Eight', 'Nine', 'Ten',
+         'Jack', 'Queen', 'King', 'Ace'
+         )
 values = {'Two':2, 'Three': 3, 'Four':4, 'Five':5, 
           'Six': 6, 'Seven':7, 'Eight': 8,
-          'Nine': 9, 'Ten': 10, 'Eleven':11,
-          'Twelve': 12, 'Thirteen':13, 'Fourteen':14
+          'Nine': 9, 'Ten': 10, 'Jack':11,
+          'Queen': 12, 'King':13, 'Ace':14
            }
 
 
-class Card():
+class Card:
 
     def __init__(self, suit, rank):
         self.suit = suit
@@ -22,4 +30,145 @@ print(two_hearts)
 print(two_hearts.suit)
 
 
-print(values[two_hearts.rank])
+class Deck: 
+
+    def __init__(self) -> None:
+        
+        self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                # Create the card object
+                created_card = Card(suit,rank)
+                
+                self.all_cards.append(created_card)
+    
+    def shuffle(self): 
+
+        random.shuffle(self.all_cards)
+    
+    def deal_one(self):
+        return self.all_cards.pop()
+
+new_deck = Deck()
+
+print(new_deck.all_cards[-1])
+
+new_deck.shuffle()
+
+print(new_deck.all_cards[-1])
+
+mycard = new_deck.deal_one()
+
+print(mycard)
+num_cards = len(new_deck.all_cards)
+print(num_cards)
+
+class Player:
+
+    def __init__(self, name) -> None:
+        
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        return self.all_cards.pop(0)
+
+    def add_cards(self, new_cards):
+        if type(new_cards) == type([]):
+            # List of multiple card objects
+            self.all_cards.extend(new_cards)
+        else: 
+            #For a single card object
+            self.all_cards.append(new_cards)
+    
+    def __str__(self) -> str:
+        return f'Player {self.name} has {len(self.all_cards)} cards'
+
+new_player = Player("Abu")
+
+print(new_player)
+
+new_player.add_cards(mycard)
+
+print(new_player)
+
+print(new_player.all_cards[0])
+
+new_player.add_cards([mycard,mycard,mycard])
+
+print(new_player)
+
+print(new_player.remove_one())
+
+print(new_player)
+
+
+# GAME SETUP
+player_one = Player("One")
+player_two = Player("Two")
+
+new_deck = Deck()
+new_deck.shuffle()
+
+for _ in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+game_on = True
+
+round_num = 0
+
+while game_on: 
+    round_num +=1
+    print(f'Round: {round_num}')
+
+    if len(player_one.all_cards) == 0:
+        print('Player one, out of cards! Player two wins!')
+        game_on = False
+        break
+    if len(player_two.all_cards) == 0:
+        print('Player two, out of cards! Player one wins!')
+        game_on = False
+        break
+
+    # START A NEW ROUND
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+
+    at_war = True
+
+    while at_war:
+
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+            
+            at_war = False
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+            
+            at_war = False
+        else: 
+            print('WAR!')
+
+            if len(player_one.all_cards) < 5 : 
+                print('Player one unable to declare war')
+                print('Player two wins!')
+                game_on = False
+                break
+
+            elif len(player_two.all_cards) < 5 : 
+                print('Player two unable to declare war')
+                print('Player one wins!')
+                game_on = False
+                break
+            else: 
+                for num in range(3):
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
